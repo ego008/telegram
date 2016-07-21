972c2b3461e735045b4f4eb24b7843ea272fd545
Kirill Danshin <k@guava.by>
2016-07-21 04:45:49 +0300

    replace net/http with fasthttp

--- getPosts.go
+++ getPosts.go
@@ -1,28 +1,29 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"log"
-	"net/http"
-	//"github.com/valyala/fasthttp" // Need to replace "net/http" later
+
+	"github.com/kirillDanshin/myutils"
+	"github.com/valyala/fasthttp"
 )
 
 // Universal(?) function obtain content
 func getPosts(tags string, pid string) []Post {
 	// JSON API with 50 results (Telegram limit)
-	repository := config.Resource[resNum].Settings.URL + "index.php?page=dapi&s=post&q=index&json=1&limit=50"
+	repository := myutils.Concat(config.Resource[resNum].Settings.URL, "index.php?page=dapi&s=post&q=index&json=1&limit=50")
 	if tags != "" {
-		repository += "&tags=" + tags // Insert tags
+		repository = myutils.Concat(repository, "&tags=", tags) // Insert tags
 	}
 	if pid != "" {
-		repository += "&pid=" + pid // Insert result-page
+		repository = myutils.Concat(repository, "&pid=", pid) // Insert result-page
 	}
-	resp, err := http.Get(repository) // Need to replace on "fasthttp" later :\
+	_, resp, err := fasthttp.Get(nil, repository)
 	if err != nil {
 		log.Printf("Error in GET request: %s", err)
 	}
-	defer resp.Body.Close()
 	var obj []Post
-	json.NewDecoder(resp.Body).Decode(&obj)
+	json.NewDecoder(bytes.NewReader(resp)).Decode(&obj)
 	return obj
 }
